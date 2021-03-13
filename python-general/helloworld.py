fbc94ce219866a12a2d570946a7bf0ac04eb1d9b
crudy002 <christopher.b.ruediger@gmail.com>
2021-03-12 23:31:38 -0500

    Initial Commit: Added sophisticated hello world math

---
+++ python-general/helloworld.py
@@ -0,0 +1,7 @@
+import random
+
+print("Hello World! \nThis is python runnning in eclipse!")
+print("Lets do some random math, shall we...?")
+randomValue1 = random.randint(1, 20)
+randomValue2 = random.randint(1, 20)
+print("%d + %d = %d" % (randomValue1, randomValue2, randomValue1+randomValue2))
